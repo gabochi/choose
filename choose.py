3a77d6adc19f2a4f849c0d69d77e2f131d291dcc
Gabriel Vinazza <28355899+gabochi@users.noreply.github.com>
2021-09-01 16:14:12 -0300

    Add files via upload

---
+++ choose.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import yaml, re, sys, os
+
+# define command index variable
+i = 0
+
+# define command list 
+cmd = ['netscape'] * 10
+
+# get last argument index
+args = len(sys.argv) - 1
+
+# get the argument, supposedly link
+string = sys.argv[args]
+
+# open YAML configuration file
+with open("config.yaml", 'r') as stream:
+    content = yaml.safe_load(stream)
+
+# iterate by browser
+for browser in content['browser']:
+
+    # increment command index and store the instruction in the cmd list
+    i+=1
+    cmd[i] = content['browser'][browser]['cmd']
+    
+    # prints index, command, link
+    print (i, cmd[i], string)
+
+    # iterate by regex to check for matches
+    for step in content['browser'][browser]['regex']:
+        match = re.search(step, string)
+
+        # there is a match
+        if match:
+            print(f"Match {browser}, running {cmd[i]} {string}")
+            
+            # build, excecute the command and quit
+            command= cmd[i] + " " + string
+            os.system(command)
+            quit()
+
+# no matches found, select browser manually
+s = int(input("No matches found, select command by number and press ENTER: "))
+command= cmd[s] + " " + string
+os.system(command)
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Choose the Browser
+Open a specific browser according to link's REGEXs.
+
+## Usage
+`choose.py <link>`
+
+- Browsers and REGEXs are defined in `config.yaml`.
+- If no match is found, you can select the browser manually.
+
+## REGEX in Python
+[Take this information in account when editing the configuration file.](https://docs.python.org/3/howto/regex.html)
+
+### TODO
+Graphical interface is to be implemented (wxPython).
